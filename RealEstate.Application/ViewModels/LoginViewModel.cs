03b65fee5b79c45bd5c46cf9567d7db0f81fc2b8
Mynevere <56651074+Mynevere@users.noreply.github.com>
2020-04-16 16:12:48 +0200

    Test Commit

--- RealEstate.Application/ViewModels/LoginViewModel.cs
+++ RealEstate.Application/ViewModels/LoginViewModel.cs
@@ -10,6 +10,8 @@ namespace RealEstate.Application.ViewModels
         [Required]
         public string Username { get; set; }
 
+        //Test Commit
+
         [Required]
         [StringLength(16, MinimumLength = 6, ErrorMessage = "You must specify a password between 6 and 16 characters")]
         public string Password { get; set; }
